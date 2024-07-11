6df3753d27d15d924bf42a0f7245d8c218ebf7da
alex-gsone <alex.gsone@gmail.com>
2024-07-10 23:00:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements(taks){
+  const ul = document.querySelector("ul");
+  let item = [tasks.length];
+  for(i=0; i<tasks.length; i++){
+    item[i] = createTaskItem(tasks[i].title, tasks[i].type)
+    ul.appendChild(item[i])
+  }
+  // ul.appendChild(item[0])
+}
+renderElements(tasks);
+
+
+
+function createTaskItem(title, type){
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  const p = document.createElement('p');
+  const button = document.createElement('button');
+
+  p.textContent=(title);
+
+  li.appendChild(div);
+  div.appendChild(span);
+  div.appendChild(p);
+  li.appendChild(button);
+
+  li.classList.add("task__item");
+  div.classList.add("task-info__container");
+  button.classList.add("task__button--remove-task");
+  
+  if(type==="Urgente"){
+    span.classList.add("task-type_span-urgent");
+  }else if(type==="Importante"){
+    span.classList.add("task-type_span-important");
+  }else{
+    span.classList.add("task-type_span-normal");
+  }
+
+  // const ul = document.querySelector("ul");
+  // ul.appendChild(li)
+  return li;
+}
+createTaskItem(tasks[3].title, tasks[3].type);
